6682350b514eb4b8d769f29c05b3acd10e898afb
Ricardo Peres <rjsperes@gmail.com>
2015-11-11 18:18:44 +0000

    Update vagrant provisioning script

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -1,29 +1,35 @@
 
 #!/usr/bin/env bash
 
-# Install Java 8.
-echo "Installing Java 8..."
+# Install Java.
+echo "Installing Java..."
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get -y update
 sudo apt-get install oracle-java8-installer
-echo "Setting environment variables for Java 8..."
 sudo apt-get install oracle-java8-set-default
+echo "Java 8 installed!"
 
-# Install Tomcat 8 web server.
-echo "Installing Tomcat..."
-sudo apt-get install -y tomcat8
+# Install Apache Tomcat.
+echo "Installing Apache Tomcat..."
+wget -P /tmp/ http://apache.mirrors.hoobly.com/tomcat/tomcat-8/v8.0.18/bin/apache-tomcat-8.0.18.tar.gz
+tar -zxvf /tmp/apache-tomcat-8.0.18.tar.gz -C /usr/share/
+echo "Apache Tomcat installed!"
 
 # Install Git.
 echo "Installing Git..."
 sudo apt-get install -y git
+echo "Git installed!"
 
 # Install Maven.
 echo "Installing Maven..."
 sudo apt-get install -y maven
-# Install Mongodb server.
-echo "Installing MongoDB..."
+echo "Maven installed!"
 
-# Add MongoDB to apt.
+# Install MongoDB.
+echo "Installing MongoDB..."
 apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get -y update
+sudo apt-get install -y mongodb-org
+echo "MongoDB installed!"
